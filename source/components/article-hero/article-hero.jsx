1520e8133e78283503e9891822cf7a3fdb2904b2
Sara <saradalfalk@gmail.com>
2019-05-23 10:38:54 +0200

    article-hero - wip

--- source/components/article-hero/article-hero.jsx
+++ source/components/article-hero/article-hero.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Heading from '../heading';
-import ResponsiveImage from '../responsive-image';
+import Image from '../image';
 import Content from '../content';
 import Row from '../row';
 
 const ArticleHero = ({ heading, image, text }) => (
-  <Content theme={Content.themes.wide}>
+  <Content theme={Content.themes.slim}>
     <Row theme={Row.themes.small}>
       <div className="article-hero">
         <Heading>{heading}</Heading>
-        <div className="article-hero__image">
-          <ResponsiveImage {...image} />
+        <div className="article-hero__image-container">
+          <Image initialSize={200} {...image} />
         </div>
-        <div>{text}</div>
+        <div className="article-hero__text-container">{text}</div>
       </div>
     </Row>
   </Content>
